df7ca79ce95181e6885a52a6b66b16377d9993f2
sumonta <110052598+sum0nta@users.noreply.github.com>
2024-01-26 01:52:55 +0600

    test.session.sql

--- test.session.sql
+++ test.session.sql
@@ -5,12 +5,3 @@ CREATE TABLE Passwords(
     pass TEXT,
     website VARCHAR(255)
 )
--- @block
-INSERT INTO Passwords(email,pass,website)
-VALUES ('xyz@bracu.ac.bd','ilikeapples12','https://www.bracu.ac.bd'),
-    ('abc@bracu.ac.bd','ilikeoranges12','https://www.bracu.ac.bd'),
-    ('mny@bracu.ac.bd','ilikedogs12','https://www.bracu.ac.bd');
-
--- @block
-SELECT email,pass,website FROM Passwords
-ORDER BY email ASC;
